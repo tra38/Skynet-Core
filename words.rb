9f86ed11053fda1b22e04531453851e0b1b73737
tra38 <tra38@users.noreply.github.com>
2015-03-30 13:16:11 -0500

    Update words.rb

--- words.rb
+++ words.rb
@@ -1,17 +1,32 @@
-def verb(trait)
+def verb(trait,target = nil)
   action = nil
-  case trait
-  when "Knowledge"
-    action = ["relied on authoritative certainty", "remembered what he taught himself","knew the held truths"].sample
-  when "Actuality"
-    action = ["knew exactly what was going on","grasped the inevitable","had already predicted this"].sample
-  when "Consider"
+  if target == nil
+    case trait
+    when "Knowledge" || "Certainity"
+    action = ["relied on authoritative certainty", "remembered what he taught himself","knew his held truths"].sample
+    when "Actuality"
+    action = ["knew exactly what was going on","grasped the inevitable","predicted this would happen"].sample
+    when "Consider"
     action = ["mulled","deliberated","contemplated","ran computer simulations","pondered","searched the lookup tables",
     "considered"].sample
-  when "Pursuit"
-    action = ["sought", "went after", "attempted to locate", "looked for", "directed efforts against"].sample
-  when "Certainity"
-    action = ["had no doubts about his knowledge", "was absolute sure", "was certain", "had conviction","knew his case was irrefutable"].sample
+    when "Pursuit"
+    action = ["investigated the situation", "examined the evidence", "looked for clues", "sought clues",
+    "frantically searched for suspects"].sample
+  end
+  else
+    case trait
+    when "Certainity" || "Knowledge"
+    action = ["boasted on his knowledge about #{target}", "was absolute sure about #{target}'s goals",
+    "was certain about #{target}", "had convictions about #{target}", "recited his irrefutable case to #{target}"].sample
+    when "Actuality"
+    action = ["blamed #{target}","identified #{target} as the guilty party"].sample
+    when "Consider"
+    action = ["identified #{target} as a threat", "classified #{target} as an enemy",
+    "identified  #{target} as a legitimate target"].sample
+    when "Pursuit"
+    action = ["sought #{target}", "went after #{target}", "attempted to locate #{target}", "looked for #{target}",
+    "directed efforts against #{target}", "pursued #{targt}"].sample
+    end
   end
   return action
 end
