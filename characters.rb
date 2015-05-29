72135959e12bee8622bdde8bb05fd444de07acde
tra38 <tra38@nau.edu>
2015-05-29 11:29:53 -0500

    Replace super with post_initialize, as described by POODR

--- characters.rb
+++ characters.rb
@@ -2,6 +2,7 @@ class Character
 attr_reader :purpose, :motivation, :methodology, :evaluation, :enemy, :total
   def initialize
     @total = @purpose+@motivation+@methodology+@evaluation
+    post_initialize
   end
 
 #Metaprogramming is the process of writing code that writes code. This
@@ -22,7 +23,7 @@ attr_reader :purpose, :motivation, :methodology, :evaluation, :enemy, :total
 end
 
 class Protagonist < Character #the "Hero", the guy who wants stuff done
-def initialize
+def post_initialize
   @purpose = ["Knowledge", "Actuality"]
   #Knowledge - dogmatism and slavish adherence to "orthodoxy"
   #Actuality - knows how the world really is
@@ -40,135 +41,126 @@ def initialize
   #Effect - cares about how to solve the problem, and does not worry about what caused the problem in the first place!
   
   @enemy = Antagonist
-  
-  super
   end
 end
 
 class Antagonist < Character #the "Villian", the guy standing in the way of the Protagonist
-def initialize
-  @purpose = ["Thought", "Perception"]
-  #Thought - Free-thinker, able to consider an issue from multiple perspectives
-  #Perception - knows only what the world SEEMS to be
-  
-  @motivation = ["Reconsider", "Avoid"]
-  #Reconsider - will periodically reassess whether the current goal is the correct course of action
-  #Avoid - desires to escape whoever is chasing him (usually the Protagonist)
-  
-  @methodology = ["Potentiality", "Reaction"]
-  #Potentiality - is unsure whether his action will lead to the correct outcome, but will still take risks when necessary
-  #Reaction - will wait until a crisis occurs, and then responds accordingly 
-  
-  @evaluation = ["Unproven, Cause"]
-  #Unproven - believes that his ideas may still be actually wrong, no matter how many previous tests are done
-  #Cause - cares about addressing the root causes of the problem, and ignores the actual problem.
-  
-  @enemy = Protagonist
-  
-  super
-end
+  def post_initialize
+    @purpose = ["Thought", "Perception"]
+    #Thought - Free-thinker, able to consider an issue from multiple perspectives
+    #Perception - knows only what the world SEEMS to be
+    
+    @motivation = ["Reconsider", "Avoid"]
+    #Reconsider - will periodically reassess whether the current goal is the correct course of action
+    #Avoid - desires to escape whoever is chasing him (usually the Protagonist)
+    
+    @methodology = ["Potentiality", "Reaction"]
+    #Potentiality - is unsure whether his action will lead to the correct outcome, but will still take risks when necessary
+    #Reaction - will wait until a crisis occurs, and then responds accordingly 
+    
+    @evaluation = ["Unproven, Cause"]
+    #Unproven - believes that his ideas may still be actually wrong, no matter how many previous tests are done
+    #Cause - cares about addressing the root causes of the problem, and ignores the actual problem.
+    
+    @enemy = Protagonist
+  end
 end
 
 class Contagonist < Character #The Tempter, out to convince others to join the "dark side"
-def initialize
-  @purpose = ["Inequity", "Speculation"]
-  #Inequity - promotes unfairness or imbalance
-  #Speculation - prepares for all potential and possible ouctomes
-  
-  @motivation = ["Temptation", "Hinder"]
-  #Temptation - embrace immedidate benefits, without worrying about the future consequences
-  #Hinder - undermining somebody else's plans, either intentionally or by accident
-  
-  @methodology = ["Production", "Reevaluation"]
-  #Production - determines what the current situation is by ruling out what explainations are "impossible"
-  #Reevaluation - continually reexamines and reinterprets the current situation
-  
-  @evaluation = ["Determination","Unending"]
-  #Determination - uses inferences to draws conclusions
-  #Unending - believes that Society can live forever and will live forever
-  
-  @enemy = Guardian
-  
-  super
-end
+  def post_initialize
+    @purpose = ["Inequity", "Speculation"]
+    #Inequity - promotes unfairness or imbalance
+    #Speculation - prepares for all potential and possible ouctomes
+    
+    @motivation = ["Temptation", "Hinder"]
+    #Temptation - embrace immedidate benefits, without worrying about the future consequences
+    #Hinder - undermining somebody else's plans, either intentionally or by accident
+    
+    @methodology = ["Production", "Reevaluation"]
+    #Production - determines what the current situation is by ruling out what explainations are "impossible"
+    #Reevaluation - continually reexamines and reinterprets the current situation
+    
+    @evaluation = ["Determination","Unending"]
+    #Determination - uses inferences to draws conclusions
+    #Unending - believes that Society can live forever and will live forever
+    
+    @enemy = Guardian
+
+  end
 end
 
 class Guardian < Character #The Moral Conscience, out to guide others to the "light side"
-def initialize
-  @purpose = ["Equity", "Projection"]
-  #Equity - promotes fairness and balance
-  #Projection - prepares for the most likely and probable outcomes
-  
-  @motivation = ["Conscience", "Help"]
-  #Conscience - forgoing immedidate benefits, because of future consequences
-  #Help - helping somebody else's plans, either intentionally or by accident
-  
-  @methodology = ["Reduction", "Evaluation"]
-  #Reduction - determines what the current situation is by weighing odds carefully and choosing the most likely explaination
-  #Evaluation - interprets the current situation to determine its meaning, and then sticks to that interpertion
-  #forever.
-  
-  @evaluation = ["Expectation", "Ending"]
-  #Expectation - anticipiates what is going to happen in the future
-  #Ending - believes that Society will end and is concerned about the aftermath
-  
-  @enemy = Contagonist
-  
-  super
-end
+  def post_initialize
+    @purpose = ["Equity", "Projection"]
+    #Equity - promotes fairness and balance
+    #Projection - prepares for the most likely and probable outcomes
+    
+    @motivation = ["Conscience", "Help"]
+    #Conscience - forgoing immedidate benefits, because of future consequences
+    #Help - helping somebody else's plans, either intentionally or by accident
+    
+    @methodology = ["Reduction", "Evaluation"]
+    #Reduction - determines what the current situation is by weighing odds carefully and choosing the most likely explaination
+    #Evaluation - interprets the current situation to determine its meaning, and then sticks to that interpertion
+    #forever.
+    
+    @evaluation = ["Expectation", "Ending"]
+    #Expectation - anticipiates what is going to happen in the future
+    #Ending - believes that Society will end and is concerned about the aftermath
+    
+    @enemy = Contagonist
+  end
 end
 
 class Reason < Character #the "Spock", uses logic and rationality in desicion-making
-def initialize
-  @purpose = ["Ability", "Aware"]
-  #Ability - competent at an assigned task
-  #Aware - knows about things outside of himself
-  
-  @motiviation = ["Logic","Control"]
-  #Logic - uses rationality to understands the world
-  #Control - focused, deliberate actions
-  
-  @methodology = ["Probablity","Inaction"]
-  #Probablity - only believes in only what is likely
-  #Inaction - prefers to take no action
-  
-  @evaluation = ["Theory","Trust"]
-  #Theory - wants to develop a fully-formed casual relationship to explain what is going on, and refuses to accept
-  #irrational appeals to "common sense"
-  #Trust - accepts without any proof (if it is logically follows from the theory, then it must be true!)
-  
-  @enemy = Emotion
-  
-  super
-end
+  def post_initialize
+    @purpose = ["Ability", "Aware"]
+    #Ability - competent at an assigned task
+    #Aware - knows about things outside of himself
+    
+    @motiviation = ["Logic","Control"]
+    #Logic - uses rationality to understands the world
+    #Control - focused, deliberate actions
+    
+    @methodology = ["Probablity","Inaction"]
+    #Probablity - only believes in only what is likely
+    #Inaction - prefers to take no action
+    
+    @evaluation = ["Theory","Trust"]
+    #Theory - wants to develop a fully-formed casual relationship to explain what is going on, and refuses to accept
+    #irrational appeals to "common sense"
+    #Trust - accepts without any proof (if it is logically follows from the theory, then it must be true!)
+    
+    @enemy = Emotion
+
+  end
 end
 
 class Emotion < Character #uses passion and intuition in desicion-making
-def initialize
-  @purpose = ["Desire","Self-Aware"]
-  #Desire - wants to improve society
-  #Self-Aware - knows about his own self
-  
-  @motivation = ["Feeling","Uncontrolled"]
-  #Feeling - can emotionally sense if something or someone is "right"
-  #Uncontrolled - will spend all his energy in all directions at once  
-  
-  @methodology = ["Possibility","Protection"]
-  #Possibility - believes in what is possible
-  #Protection - will take action to defend himself from possible threats
+  def post_initialize
+    @purpose = ["Desire","Self-Aware"]
+    #Desire - wants to improve society
+    #Self-Aware - knows about his own self
+    
+    @motivation = ["Feeling","Uncontrolled"]
+    #Feeling - can emotionally sense if something or someone is "right"
+    #Uncontrolled - will spend all his energy in all directions at once  
+    
+    @methodology = ["Possibility","Protection"]
+    #Possibility - believes in what is possible
+    #Protection - will take action to defend himself from possible threats
 
-  @evaluation = ["Hunch","Test"]
-  #Hunch - uses intuition to determine what is going on, without need for such petty things like actual evdience
-  #Test - wants to conduct trials and investigations to determine if something is true (using those oh-so-reliable hunches)
-  
-  @enemy = Reason
-  
-  super
-end
+    @evaluation = ["Hunch","Test"]
+    #Hunch - uses intuition to determine what is going on, without need for such petty things like actual evdience
+    #Test - wants to conduct trials and investigations to determine if something is true (using those oh-so-reliable hunches)
+    
+    @enemy = Reason
+    
+  end
 end
 
 class Sidekick < Character #a die-hard supporter of another character's actions and goals
-  def initialize
+  def post_initialize
     @purpose = ["Order","Inertia"]
     #Order - Supports stability and organization
     #Inertia - Preference for the status quo
@@ -186,13 +178,12 @@ class Sidekick < Character #a die-hard supporter of another character's actions 
     #Result - cares about the outcomes and consequences of actions
     
     @enemy = Skeptic
-    
-    super
+
   end
 end
 
 class Skeptic < Character #the "Doubter", thinks his boss does not know what he is doing
-  def initialize
+  def post_initialize
      @purpose = ["Chaos","Change"]
     #Order - Supports random change and disorder
     #Change - Wants the status quo overturned
@@ -210,7 +201,6 @@ class Skeptic < Character #the "Doubter", thinks his boss does not know what he 
     #Process - cares about making sure actions are done properly, with no worry about the consequences of said action.
     
     @enemy = Sidekick
-    
-    super
+
   end
 end
